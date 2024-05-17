9b65e0c335af401d9e006f80f75cfdc10560a0a9
tomerab <tomer1452002@gmail.com>
2024-05-18 00:21:18 +0300

    Initial

---
+++ app/main.cpp
@@ -0,0 +1,72 @@
+// clang-format off
+#include <glad/glad.h>
+// clang-format on
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+struct RGBColor
+{
+    float r, g, b, alpha;
+};
+
+RGBColor getRGB(int r, int g, int b, float alpha)
+{
+    return RGBColor{r / 255.f, g / 255.f, b / 255.f, alpha};
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Learnopengl", nullptr, nullptr);
+    if (!window)
+    {
+        std::cout << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+    glfwSetKeyCallback(window, keyCallback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        const auto [r, g, b, a] = getRGB(139, 173, 176, 1.f);
+
+        glClearColor(r, g, b, a);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
